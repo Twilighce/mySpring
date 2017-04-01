421e6a097ea96d11d34de1debfc7b6deeee33b5d
rbing <rbing@mail.ustc.edu.cn>
2017-04-01 20:27:46 +0800

    v1

---
+++ src/com/Twilighce/dao/impl/UserDAOImpl.java
@@ -0,0 +1,17 @@
+package com.Twilighce.dao.impl;
+
+import com.Twilighce.dao.UserDAO;
+import com.Twilighce.model.User;
+
+
+public class UserDAOImpl implements UserDAO {
+
+	public void save(User user) {
+		//Hibernate
+		//JDBC
+		//XML
+		//NetWork
+		System.out.println("user saved!");
+	}
+
+}
